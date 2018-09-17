f8dc61c58f04c7e723e262926caebb88fff12b67
李刚 <867982914@QQ.com>
2018-09-17 09:56:48 +0800

    字体修改

--- js/seek.global.js
+++ js/seek.global.js
@@ -6,9 +6,9 @@ const STATUS_OK = 200;
 const SUCCESS = 'success';
 const FAIL = 'fail';
 const TEN = 10;
-// const BASEURL = 'http://192.168.0.102:8080/seek01/';
-const BASEURL = 'http://192.168.0.177:8888/seek01/'
-
+   const BASEURL = 'http://192.168.0.103:8080/seek01/';
+//const BASEURL = 'http://192.168.0.177:8888/seek01/'
+//const BASEURL = 'http://www.want-tech.com/seek01/'
 //新建axios实例,普通form表单
 var simpleAxios = axios.create({
 	baseURL : BASEURL,
