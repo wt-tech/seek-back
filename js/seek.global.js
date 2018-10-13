b557f958b813cba80f2de46cce2cca7e8063cc41
cgrezo <2422687506@qq.com>
2018-10-13 15:50:28 +0800

    更新

--- js/seek.global.js
+++ js/seek.global.js
@@ -11,7 +11,7 @@ const TEN = 10;
 
 
 //const BASEURL = 'http://192.168.0.177:8888/seek/'
-const BASEURL = 'http://192.168.0.109:8080/seek01/'
+const BASEURL = 'http://192.168.0.109:8080/seek/'
 //const BASEURL = 'https://www.qghls.com/seek/'
 
 
